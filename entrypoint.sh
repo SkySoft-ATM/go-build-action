6abbee11ac8230ce11e77a77f39f4497ed236312
matgabriel <mgabriel@mgabriel.com>
2020-03-02 11:54:03 +0100

    add docker login

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,12 +7,14 @@ GITHUB_USER=$2
 GITHUB_TOKEN=$3
 PROJECT=$4
 GOSEC_OPTS=$5
+GCLOUD_TOKEN=$6
 
 if [ -z "${DOCKERFILE}" ]; then echo "::error ::Undefined dockerfile" && exit 1; fi
 if [ -z "${GITHUB_USER}" ]; then echo "::error ::Undefined github user" && exit 1; fi
 if [ -z "${GITHUB_TOKEN}" ]; then echo "::error ::Undefined github token" && exit 1; fi
 if [ -z "${PROJECT}" ]; then echo "::error ::Undefined project" && exit 1; fi
 
+echo "${GCLOUD_TOKEN}" | base64 -d | docker login -u _json_key --password-stdin https://eu.gcr.io
 
 docker build --build-arg GITHUB_USER="${GITHUB_USER}" --build-arg GITHUB_TOKEN="${GITHUB_TOKEN}" --build-arg PROJECT="${PROJECT}" --build-arg GOSEC_OPTS="${GOSEC_OPTS}" . -f "${DOCKERFILE}"
 
